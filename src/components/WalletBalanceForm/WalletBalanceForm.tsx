51d31ec25c9d76bd0870231228c7f186d719c045
Serhii Prysiazhnyi <serge.prysiazhnyi@gmail.com>
2024-10-19 18:00:54 +0200

    add address validation check in WalletBalanceForm

--- src/components/WalletBalanceForm/WalletBalanceForm.tsx
+++ src/components/WalletBalanceForm/WalletBalanceForm.tsx
@@ -7,27 +7,42 @@ import { ErrorNotification } from '../ErrorNotification/ErrorNotification';
 
 import styles from './WalletBalanceForm.module.css';
 
+const isValidETHAddress = (address: string) => {
+    return /^0x[a-fA-F0-9]{40}$/.test(address);
+};
+
 export const WalletBalanceForm = () => {
     const [inputValue, setInputValue] = useState('');
+    const [validationError, setValidationError] = useState<string | null>(null);
 
-    const { fetchBalance, loading, error, balance } =
-        useFetchEthereumAddressBalance();
+    const {
+        fetchBalance,
+        loading,
+        error: fetchError,
+        balance,
+    } = useFetchEthereumAddressBalance();
 
     const handleSubmit = async (e: JSX.TargetedEvent<HTMLFormElement>) => {
         e.preventDefault();
-        await fetchBalance(inputValue);
+        setValidationError(null);
+
+        if (inputValue && isValidETHAddress(inputValue)) {
+            await fetchBalance(inputValue);
+        } else {
+            setValidationError('Please provide a valid Ethereum address');
+        }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className={styles.form}>
             <input
                 id="address"
                 className={styles.addressInput}
                 type="text"
                 value={inputValue}
-                onChange={(e: JSX.TargetedEvent<HTMLInputElement>) =>
-                    setInputValue(e.currentTarget.value)
-                }
+                onChange={(e: JSX.TargetedEvent<HTMLInputElement>) => {
+                    setInputValue(e.currentTarget.value);
+                }}
                 placeholder="Enter ETH wallet address to get balance"
             />
 
@@ -39,11 +54,11 @@ export const WalletBalanceForm = () => {
                 {loading ? <LoadingIndicator /> : 'Submit'}
             </button>
 
-            {balance && (
+            {balance && !validationError && !fetchError && (
                 <p className={styles.balance}>{`Balance: ${balance} ETH`}</p>
             )}
 
-            <ErrorNotification message={error} />
+            <ErrorNotification message={validationError || fetchError} />
         </form>
     );
 };
---
+++ src/utils/isValidETHAddress.ts
@@ -0,0 +1,3 @@
+export const isValidETHAddress = (address: string) => {
+    return /^0x[a-fA-F0-9]{40}$/.test(address);
+};
